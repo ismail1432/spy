8ba37d073c467521cf21b20971b599aec293d104
Smaine Milianni <smaine.milianni@gmail.com>
2020-04-23 01:57:35 +0100

    more cs

--- src/Assertion/SpyAssertion.php
+++ src/Assertion/SpyAssertion.php
@@ -4,14 +4,31 @@ namespace Eniams\Spy\Assertion;
 
 use Eniams\Spy\Exception\UncomparableException;
 
+/**
+ * @author Smaïne Milianni <contact@smaine.me>
+ */
 final class SpyAssertion
 {
-    public static function sameFqcn($firstObject, $secondObject)
+    /**
+     * Full qualified namespace comparaison.
+     *
+     * @param object $firstObject
+     * @param object $secondObject
+     */
+    public static function sameFqcn($firstObject, $secondObject): bool
     {
         return get_class($firstObject) === get_class($secondObject);
     }
 
-    public static function isComparable($firstObject, $secondObject)
+    /**
+     * Check if the both given object can be compared.
+     *
+     * @param object $firstObject
+     * @param object $secondObject
+     *
+     * @throws UncomparableException
+     */
+    public static function isComparable($firstObject, $secondObject): bool
     {
         if (self::sameFqcn($firstObject, $secondObject)) {
             return true;
